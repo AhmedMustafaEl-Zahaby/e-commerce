b151cc11d17dee1d4fc5a5844cb77a34144bdf55
ahmedelzahaby <ahmed.elzahaby@bld.ai>
2024-04-28 06:02:23 +0300

    add resposnive for navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
-import React, { useContext } from "react";
-import { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
-import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Contexts/ShopContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
+
 function Navbar() {
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <div className="navbar">
       <Link to="/" style={{ textDecoration: "none" }}>
@@ -16,41 +23,86 @@ function Navbar() {
           <p>SHOPPER</p>
         </div>
       </Link>
-      <ul className="nav-menu">
-        <li onClick={() => setMenu("shop")}>
+      <ul className={`nav-menu ${showMenu ? "show" : ""}`}>
+        <li
+          onClick={() => {
+            setMenu("shop");
+            if (window.screen.width < 970) {
+              toggleMenu();
+            }
+          }}
+        >
           <Link to="/" style={{ textDecoration: "none", color: "black" }}>
             Shop
           </Link>{" "}
           {menu === "shop" && <hr />}
         </li>
-        <li onClick={() => setMenu("men")}>
+        <li
+          onClick={() => {
+            setMenu("men");
+            if (window.screen.width < 970) {
+              toggleMenu();
+            }
+          }}
+        >
           <Link to="/men" style={{ textDecoration: "none", color: "black" }}>
             Men
           </Link>{" "}
           {menu === "men" && <hr />}
         </li>
-        <li onClick={() => setMenu("women")}>
-          <Link to="women" style={{ textDecoration: "none", color: "black" }}>
+        <li
+          onClick={() => {
+            setMenu("women");
+            if (window.screen.width < 970) {
+              toggleMenu();
+            }
+          }}
+        >
+          <Link to="/women" style={{ textDecoration: "none", color: "black" }}>
             Women
           </Link>{" "}
           {menu === "women" && <hr />}
         </li>
-        <li onClick={() => setMenu("kids")}>
-          <Link to="kids" style={{ textDecoration: "none", color: "black" }}>
+        <li
+          onClick={() => {
+            setMenu("kids");
+            if (window.screen.width < 970) {
+              toggleMenu();
+            }
+          }}
+        >
+          <Link to="/kids" style={{ textDecoration: "none", color: "black" }}>
             Kids
           </Link>{" "}
           {menu === "kids" && <hr />}
         </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+            if (window.screen.width < 970) {
+              toggleMenu();
+            }
+          }}
+        >
+          <Link to="login" className="mobile">
+            <button>Login</button>
+          </Link>
+        </li>
       </ul>
       <div className="nav-login-cart">
         <Link to="login">
-          <button>Login</button>
+          <button className={`screen`}>Login</button>
         </Link>
         <Link to="/cart">
           <img src={cart_icon} alt="cart" />
         </Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
+        <div className="bars-icon" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} size="2x" />
+        </div>
       </div>
+      {/* Overlay for responsive design */}
+      {showMenu && <div className="overlay" onClick={toggleMenu}></div>}
     </div>
   );
 }
